4c39a4ea323b95544c3e1626d6b73c0eed291e69
Alexander Nefedyev <knighter@rambler.ru>
2022-08-20 14:19:03 +0900

    Init commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Map from '../Map/Map';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Map />
+        </div>
+    );
+}
+
+export default App;
